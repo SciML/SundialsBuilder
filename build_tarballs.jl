b0d352ea078d7d76253c374d2557f8c8ba817d2b
Christopher Rackauckas <accounts@chrisrackauckas.com>
2018-01-14 16:11:38 -0800

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -32,7 +32,7 @@ cd $WORKSPACE/srcdir/sundials-3.1.0/
 mkdir build
 cd build
 
-if [ $target = "i686-w64-mingw32" ]; then
+if [ $target = i686-* ] -o [ $target = arm-* ]; then
 cmake -DCMAKE_INSTALL_PREFIX=/ -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_BUILD_TYPE=Release -DEXAMPLES_ENABLE=OFF -DSUNDIALS_INDEX_TYPE=int32_t ..
 else
 cmake -DCMAKE_INSTALL_PREFIX=/ -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DCMAKE_BUILD_TYPE=Release -DEXAMPLES_ENABLE=OFF ..
